8c2d77da0728ab9094c8e3a456a8135d241299c6
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2013-12-24 16:43:31 +0200

    changed binary paths in build scripts

--- csrc/libexif/build-mingw32.sh
+++ csrc/libexif/build-mingw32.sh
@@ -1,3 +1,3 @@
-gcc -s -O3 -shared -static-libgcc -o ../../bin/exif.dll -g -Wall libexif/*.c libexif/*/*.c -I. -D__WATCOMC__
+gcc -s -O3 -shared -static-libgcc -o ../../bin/mingw32/exif.dll -g -Wall libexif/*.c libexif/*/*.c -I. -D__WATCOMC__
 
-cd ../.. && bin/luajit libexif.lua
+#cd ../.. && bin/luajit libexif.lua
