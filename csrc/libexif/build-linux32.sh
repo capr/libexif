daf229cb442b74a8d8612a827419836ac3a1008e
Cosmin Apreuetsei <cosmin.apreutesei@gmail.com>
2014-01-23 00:51:35 +0200

    removed CFLAGS/CXXFLAGS

--- csrc/libexif/build-linux32.sh
+++ csrc/libexif/build-linux32.sh
@@ -1,3 +1,3 @@
-gcc $CFLAGS libexif/*.c libexif/*/*.c -shared -o ../../bin/linux32/libexif.so -Wall -I. -D__WATCOMC__
+gcc -O2 -s -static-libgcc libexif/*.c libexif/*/*.c -shared -o ../../bin/linux32/libexif.so -Wall -I. -D__WATCOMC__
 
 #cd ../.. && linux/bin/luajit libexif.lua
--- csrc/libexif/build-mingw32.sh
+++ csrc/libexif/build-mingw32.sh
@@ -1,3 +1,3 @@
-gcc $CFLAGS libexif/*.c libexif/*/*.c -shared -o ../../bin/mingw32/exif.dll -Wall -I. -D__WATCOMC__
+gcc -O2 -s -static-libgcc libexif/*.c libexif/*/*.c -shared -o ../../bin/mingw32/exif.dll -Wall -I. -D__WATCOMC__
 
 #cd ../.. && bin/luajit libexif.lua
