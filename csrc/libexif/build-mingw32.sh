64c10ca87cb52520e2af811ac2899c3ddbddcc33
Cosmin Apreuetsei <cosmin.apreutesei@gmail.com>
2014-02-01 00:53:31 +0200

    bugfix in build script

--- csrc/libexif/build-mingw32.sh
+++ csrc/libexif/build-mingw32.sh
@@ -1,2 +1,2 @@
-gcc -O2 -s -static-libgcc libexif/*.c libexif/*/*.c -shared -o ../../bin/mingw32/exif.dll -Wall -I. \
+gcc -O2 -s -static-libgcc libexif/*.c libexif/*/*.c -shared -o ../../bin/mingw32/exif.dll -I. \
     -DHAVE_GETTEXT=1 -DGETTEXT_PACKAGE=\"libexif\" -DLOCALEDIR=\"UTF-8\"
